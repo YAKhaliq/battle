1b565905f86cf633a22dce9bd1f95352fa6bd8bb
Yusuf <yakhaliq01@gmail.com>
2021-06-14 15:43:26 +0100

    returning HTML

---
+++ app.rb
@@ -0,0 +1,16 @@
+require 'sinatra'
+require 'sinatra/reloader' if development?
+
+get '/' do
+  "Hello!"
+end
+
+get '/secret' do
+  'this is secret'
+end
+
+get '/cat' do
+  "<div style= 'border: 5px dashed red'>
+    <img src='https://i.imgur.com/jFaSxym.png'>
+  <div>"
+end
